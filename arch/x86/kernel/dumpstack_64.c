c01df53524760c6fb983ade3878680f3c6083054
Josh Poimboeuf <jpoimboe@redhat.com>
2016-07-18 22:29:18 -0500

    x86/dumpstack: add IRQ_USABLE_STACK_SIZE define
    For reasons unknown, the x86_64 irq stack starts at an offset 64 bytes
    from the end of the page.  At least make that explicit.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- arch/x86/kernel/dumpstack_64.c
+++ arch/x86/kernel/dumpstack_64.c
@@ -103,9 +103,6 @@ in_irq_stack(unsigned long *stack, unsigned long *irq_stack,
 	return (stack >= irq_stack && stack < irq_stack_end);
 }
 
-static const unsigned long irq_stack_size =
-	(IRQ_STACK_SIZE - 64) / sizeof(unsigned long);
-
 enum stack_type {
 	STACK_IS_UNKNOWN,
 	STACK_IS_NORMAL,
@@ -133,7 +130,7 @@ analyze_stack(int cpu, struct task_struct *task, unsigned long *stack,
 		return STACK_IS_NORMAL;
 
 	*stack_end = irq_stack;
-	irq_stack = irq_stack - irq_stack_size;
+	irq_stack -= (IRQ_USABLE_STACK_SIZE / sizeof(long));
 
 	if (in_irq_stack(stack, irq_stack, *stack_end))
 		return STACK_IS_IRQ;
