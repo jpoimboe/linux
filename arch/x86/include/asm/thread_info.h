f4cc5e1d158360bef5d1feb95b89bec6281941ab
Josh Poimboeuf <jpoimboe@redhat.com>
2016-08-16 14:35:39 -0500

    x86/mm/usercopy: simplify starting frame logic
    Currently, arch_within_stack_frames() has to manually find the stack
    frame of its great-grandparent (i.e., it's caller's caller's caller).
    This is somewhat fragile because it relies on the current call path and
    inlining decisions.
    
    Get the starting frame address closer to the source, in
    check_stack_object(), and pass it to arch_within_stack_frames().
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- arch/x86/include/asm/thread_info.h
+++ arch/x86/include/asm/thread_info.h
@@ -178,10 +178,12 @@ static inline unsigned long current_stack_pointer(void)
 #ifdef CONFIG_FRAME_POINTER
 int arch_within_stack_frames(const void * const stack,
 			     const void * const stackend,
+			     void *first_frame,
 			     const void *obj, unsigned long len);
 #else
 static inline int arch_within_stack_frames(const void * const stack,
 					   const void * const stackend,
+					   void *first_frame
 					   const void *obj, unsigned long len)
 {
 	return 0;
